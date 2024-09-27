a64311d4748fdf5ac42fb53586c3ba163d910c8e
azimjonking <azimjonjalilov1301@mail.ru>
2024-09-27 10:35:38 +0300

    fetch post method

--- js/fetch.js
+++ js/fetch.js
@@ -1,11 +1,18 @@
-const btn = document.getElementById("get");
+const btn = document.getElementById("post");
 
 btn.addEventListener("click", () => {
   fetch("http://localhost:3000/products", {
-    method: "GET",
+    method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
+    body: JSON.stringify({
+      title: "Buyuklik sari...",
+      author: "Azimjon Jalilov",
+      genre: "autobiographic",
+      publicationYear: "2024",
+      rating: 4.8,
+    }),
   })
     .then((data) => data.json())
     .then((data) => console.log(data))
